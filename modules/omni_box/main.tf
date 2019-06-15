1be72fbecb96ec17b26681050424cc52ae3c7479
Joseph D. Marhee <jmarhee@interiorae.com>
2019-06-15 17:26:03 -0500

    fixes output file; removes defunct kv

--- modules/omni_box/main.tf
+++ modules/omni_box/main.tf
@@ -18,8 +18,3 @@ resource "aws_instance" "omni_box" {
     Name = "${format("omni-${var.lab_name}-%02d", count.index)}"
   }
 }
-
-output "instances" {
-  description = "AWS Instances created in lab"
-  value       = "${join("\t\n", aws_instance.omni_box.*.public_dns)}"
-}
--- modules/omni_box/output.tf
+++ modules/omni_box/output.tf
@@ -1,3 +1,4 @@
-output "agent-ip" {
-  value = "${join(",", aws_instance.omni_box.*.public_ip)}"
+output "instances" {
+  description = "AWS Instances created in lab"
+  value       = "${join("\t\n", aws_instance.omni_box.*.public_dns)}"
 }
