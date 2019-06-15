b9f1513fda4896276a3bf588e7cbb71bd29f6236
Joseph D. Marhee <jmarhee@interiorae.com>
2019-06-15 17:58:37 -0500

    adds securitygroup config, for SSH, updates output to be more readable

--- modules/omni_box/main.tf
+++ modules/omni_box/main.tf
@@ -9,12 +9,34 @@ resource "aws_key_pair" "lab" {
   public_key = "${file("${var.public_key_path}")}"
 }
 
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "omni_box" {
-  ami           = "${var.ami}"
-  key_name      = "${aws_key_pair.lab.key_name}"
-  instance_type = "${var.instance_type}"
-  count         = "${var.node_count}"
+  ami             = "${var.ami}"
+  key_name        = "${aws_key_pair.lab.key_name}"
+  instance_type   = "${var.instance_type}"
+  count           = "${var.node_count}"
+  security_groups = ["${aws_security_group.allow_ssh.name}"]
   tags = {
     Name = "${format("omni-${var.lab_name}-%02d", count.index)}"
   }
 }
+
+
--- modules/omni_box/output.tf
+++ modules/omni_box/output.tf
@@ -1,4 +1,4 @@
 output "instances" {
   description = "AWS Instances created in lab"
-  value       = "${join("\t\n", aws_instance.omni_box.*.public_dns)}"
+  value       = "\n\t\n\tssh root@${join("\n\t\n\tssh root@", aws_instance.omni_box.*.public_dns)}"
 }
